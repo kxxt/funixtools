309b6afc0898578dfa6fbb9f09992fc9d9220752
kxxt <rsworktech@outlook.com>
2023-08-28 21:35:30 +0800

    feat: add set{diff,union,inter} command

---
+++ src/bin/setdiff.rs
@@ -0,0 +1,24 @@
+use clap::Parser;
+use color_eyre::eyre::Result;
+use itertools::join;
+use std::{collections::BTreeSet, fs};
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[clap(short, long, default_value_t=String::from("\n"))]
+    separator: String,
+    left: String,
+    right: String,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    color_eyre::install()?;
+    let left_input = fs::read_to_string(cli.left)?;
+    let right_input = fs::read_to_string(cli.right)?;
+    let left_set: BTreeSet<_> = left_input.split(&cli.separator).collect();
+    let right_set: BTreeSet<_> = right_input.split(&cli.separator).collect();
+    let diff = left_set.difference(&right_set);
+    println!("{}", join(diff, &cli.separator));
+    Ok(())
+}
